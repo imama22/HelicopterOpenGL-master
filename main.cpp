bd6d347d6ba745666c224eb24388c6f6cea3635e
imama22 <khuyenkhung1991>
2014-11-14 08:54:00 +0100

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//******************** HELICOPTER ANIMATION *************************************//
+// A program to draw a simple helicopter and
+
+#include "Helicopter.h"
+
+int main(int argc, char* args[])
+{
+    Helicopter helicopter;      // create instance of a helicopter
+    helicopter.draw();          // starts drawing a helicopter
+    return 0;
+}
